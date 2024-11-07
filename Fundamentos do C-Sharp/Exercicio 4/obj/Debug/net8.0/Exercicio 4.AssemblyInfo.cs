8203dbf0080ac5b36772637fdef60e6cc55866a5
Milani <g.milani.e@gmail.com>
2024-11-07 17:04:32 -0300

    Corrigido Nome do projeto de tarefa 1

--- Fundamentos do C-Sharp/Exercicio 4/obj/Debug/net8.0/Exercicio 4.AssemblyInfo.cs
+++ Fundamentos do C-Sharp/Exercicio 4/obj/Debug/net8.0/Exercicio 4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio 4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9892098971e3be57947ed805371348c13630d945")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio 4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio 4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
